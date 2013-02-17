8d7bfc5c8aa6aa0ff10e312ca6e640364f978a99
George Brocklehurst <george.brocklehurst@gmail.com>
2013-02-17 12:22:29 +0100

    Refactor EncryptionKey.

--- onepassword/encryption_key.py
+++ onepassword/encryption_key.py
@@ -21,36 +21,50 @@ class SaltyString(object):
 class EncryptionKey(object):
     MINIMUM_ITERATIONS = 1000
 
-    def __init__(self, data, iterations=0, validation=""):
-        self.data = SaltyString(data)
+    def __init__(self, data, iterations=0, validation="", identifier=None,
+                 level=None):
+        self._encrypted_key = SaltyString(data)
+        self._decrypted_key = None
         self._set_iterations(iterations)
-        self._validation = SaltyString(validation)
+        self._validation = validation
 
-    def decrypt(self, password):
-        derived_key, derived_init_vector = self._derive(password)
-        aes = AES.new(derived_key, mode=AES.MODE_CBC, IV=derived_init_vector)
-        decrypted_key = aes.decrypt(self.data.data)
-        if self._validate(decrypted_key):
-            return decrypted_key
+    def unlock(self, password):
+        key, iv = self._derive_pbkdf2(password)
+        self._decrypted_key = self._aes_decrypt(
+            key=key,
+            iv=iv,
+            encrypted_data=self._encrypted_key.data,
+        )
+        return self._validate_decrypted_key()
+
+    def decrypt(self, b64_data):
+        encrypted = SaltyString(b64_data)
+        key, iv = self._derive_openssl(self._decrypted_key, encrypted.salt)
+        return self._aes_decrypt(key=key, iv=iv, encrypted_data=encrypted.data)
 
     def _set_iterations(self, iterations):
         self.iterations = max(int(iterations), self.MINIMUM_ITERATIONS)
 
-    def _derive(self, password):
-        derived_key_and_iv = pbkdf2_bin(password, self.data.salt, self.iterations,
-                                        keylen=32)
+    def _validate_decrypted_key(self):
+        return self.decrypt(self._validation) == self._decrypted_key
+
+    def _aes_decrypt(self, key, iv, encrypted_data):
+        aes = AES.new(key, mode=AES.MODE_CBC, IV=iv)
+        return aes.decrypt(encrypted_data)
+
+    def _derive_pbkdf2(self, password):
+        key_and_iv = pbkdf2_bin(
+            password,
+            self._encrypted_key.salt,
+            self.iterations,
+            keylen=32,
+        )
         return (
-            derived_key_and_iv[0:16],
-            derived_key_and_iv[16:],
+            key_and_iv[0:16],
+            key_and_iv[16:],
         )
 
-    def _validate(self, decrypted_key):
-        key, iv = self._parse_open_ssl_key(decrypted_key, self._validation.salt)
-        aes = AES.new(key, mode=AES.MODE_CBC, IV=iv)
-        verification = aes.decrypt(self._validation.data)
-        return verification == decrypted_key
-
-    def _parse_open_ssl_key(self, key, salt):
+    def _derive_openssl(self, key, salt):
         key = key[0:-16]
         key_and_iv = ""
         prev = ""
