0536d283733ec98c522a937b241a6a788165706d
George Brocklehurst <george.brocklehurst@gmail.com>
2013-02-17 12:44:04 +0100

    KeyChain can load EncryptionKeys.

--- onepassword/keychain.py
+++ onepassword/keychain.py
@@ -1,6 +1,28 @@
+import json
+import os
+
+from onepassword.encryption_key import EncryptionKey
+
+
 class Keychain(object):
-    def __init__(self, data_directory):
-        pass
+    def __init__(self, path):
+        self._path = path
+        self._load_encryption_keys()
 
     def unlock(self, password):
+        unlocker = lambda key: key.unlock(password)
+        unlock_results = map(unlocker, self._encryption_keys.values())
+        return reduce(lambda x, y: x and y, unlock_results)
+
+    def item(self, name):
         pass
+
+    def _load_encryption_keys(self):
+        path = os.path.join(self._path, "data", "default", "encryptionKeys.js")
+        with open(path, "r") as f:
+            key_data = json.load(f)
+
+        self._encryption_keys = {}
+        for key_definition in key_data["list"]:
+            level = key_definition["level"]
+            self._encryption_keys[level] = EncryptionKey(**key_definition)
--- tests/integration_test.py
+++ tests/integration_test.py
@@ -6,8 +6,8 @@ from onepassword import Keychain
 
 class IntegrationTest(TestCase):
     def test_unlock_and_read_password(self):
-        path = os.path.join(os.path.dirname(__file__), "data")
-        keychain = Keychain(data_directory=path)
+        path = os.path.join(os.path.dirname(__file__), "data", "1Password.agilekeychain")
+        keychain = Keychain(path=path)
 
         unlock_result = keychain.unlock("wrong-password")
         self.assertFalse(unlock_result)
@@ -15,6 +15,6 @@ class IntegrationTest(TestCase):
         unlock_result = keychain.unlock("badger")
         self.assertTrue(unlock_result)
 
-        self.assertEquals("123456", keychain.get_password("onetosix"))
-        self.assertEquals("abcdef", keychain.get_password("atof"))
-        self.assertNone(keychain.get_password("does-not-exist"))
+        self.assertIsNone(keychain.item("does-not-exist"))
+        self.assertEquals("123456", keychain.item("onetosix").password)
+        self.assertEquals("abcdef", keychain.item("atof").password)
