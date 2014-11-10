d66a7fd537f43962e88e27591dafe450eb1e280f
George Hickman <george@ghickman.co.uk>
2014-11-10 22:51:18 +0000

    Refactor CLI class into function

--- onepassword/cli.py
+++ onepassword/cli.py
@@ -1,89 +1,40 @@
-import argparse
 import getpass
 import os
 import sys
 
-from onepassword import Keychain
+import click
+
+from .keychain import Keychain
 
 DEFAULT_KEYCHAIN_PATH = "~/Dropbox/1Password.agilekeychain"
 
-class CLI(object):
-    """
-    The 1pass command line interface.
-    """
 
-    def __init__(self, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
-                 getpass=getpass.getpass, arguments=sys.argv[1:]):
-        self.stdin = stdin
-        self.stdout = stdout
-        self.stderr = stderr
-        self.getpass = getpass
-        self.arguments = self.argument_parser().parse_args(arguments)
-        self.keychain = Keychain(self.arguments.path)
+@click.command()
+@click.argument('item')
+@click.option('--path', envvar='ONEPASSWORD_KEYCHAIN', default=DEFAULT_KEYCHAIN_PATH, help="Path to your 1Password.agilekeychain file")
+@click.option('--fuzzy', is_flag=True, help="Perform fuzzy matching on the item")
+@click.option('--no-prompt', is_flag=True, help="Don't prompt for a password, read from STDIN instead")
+def cli(item, path, fuzzy, no_prompt):
+    keychain = Keychain(path)
 
-    def run(self):
-        """
-        The main entry point, performs the appropriate action for the given
-        arguments.
-        """
-        self._unlock_keychain()
-
-        item = self.keychain.item(
-            self.arguments.item,
-            fuzzy_threshold=self._fuzzy_threshold(),
-        )
-
-        if item is not None:
-            self.stdout.write("%s\n" % item.password)
-        else:
-            self.stderr.write("1pass: Could not find an item named '%s'\n" % (
-                self.arguments.item,
-            ))
+    if no_prompt:
+        password = sys.stdin.read().strip()
+        keychain.unlock(password)
+        if keychain.locked:
+            sys.stderr.write("1pass: Incorrect master password\n")
             sys.exit(os.EX_DATAERR)
-
-    def argument_parser(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("item", help="The name of the password to decrypt")
-        parser.add_argument(
-            "--path",
-            default=os.environ.get('ONEPASSWORD_KEYCHAIN', DEFAULT_KEYCHAIN_PATH),
-            help="Path to your 1Password.agilekeychain file",
-        )
-        parser.add_argument(
-            "--fuzzy",
-            action="store_true",
-            help="Perform fuzzy matching on the item",
-        )
-        parser.add_argument(
-            "--no-prompt",
-            action="store_true",
-            help="Don't prompt for a password, read from STDIN instead",
-        )
-        return parser
-
-    def _unlock_keychain(self):
-        if self.arguments.no_prompt:
-            self._unlock_keychain_stdin()
-        else:
-            self._unlock_keychain_prompt()
-
-    def _unlock_keychain_stdin(self):
-        password = self.stdin.read().strip()
-        self.keychain.unlock(password)
-        if self.keychain.locked:
-            self.stderr.write("1pass: Incorrect master password\n")
-            sys.exit(os.EX_DATAERR)
-
-    def _unlock_keychain_prompt(self):
-        while self.keychain.locked:
+    else:
+        while keychain.locked:
             try:
-                self.keychain.unlock(self.getpass("Master password: "))
+                keychain.unlock(getpass("Master password: "))
             except KeyboardInterrupt:
-                self.stdout.write("\n")
+                sys.stdout.write("\n")
                 sys.exit(0)
 
-    def _fuzzy_threshold(self):
-        if self.arguments.fuzzy:
-            return 70
-        else:
-            return 100
+    item = keychain.item(item, 70 if fuzzy else 100)
+
+    if item is not None:
+        sys.stdout.write("%s\n" % item.password)
+    else:
+        sys.stderr.write("1pass: Could not find an item named '%s'\n" % (item))
+        sys.exit(os.EX_DATAERR)
