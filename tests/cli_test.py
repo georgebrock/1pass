2a38a2ac1ca2f9f29f5534c8c4536278b4100201
George Hickman <george@ghickman.co.uk>
2014-11-11 08:53:43 +0000

    Rebuild cli tests for click usage
    Switch the tests to use the click based cli function and use clicks
    testing helpers where possible.

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,116 +1,65 @@
 import os
 from unittest import TestCase
-from StringIO import StringIO
 
-from onepassword.cli import CLI
+from click.testing import CliRunner
+from mock import patch
+
+from onepassword.cli import cli
 
 
 class CLITest(TestCase):
     def setUp(self):
-        self.output = StringIO()
-        self.error = StringIO()
-        self.input = StringIO()
+        self.runner = CliRunner()
 
     def test_cli_reading_web_form_password_with_multiple_password_attempts(self):
         password_attempts = (i for i in ("incorrect", "badger"))
-        cli = self.build_cli(
-            getpass=lambda prompt: password_attempts.next(),
-            arguments=("--path", self.keychain_path, "onetosix",),
-        )
-        cli.run()
+        with patch('getpass.getpass', lambda prompt: password_attempts.next()):
+            args = ['--path', self.keychain_path, 'onetosix']
+            result = self.runner.invoke(cli, args)
 
-        self.assert_output("123456\n")
-        self.assert_no_error_output()
+        assert result.exit_code == 0
+        assert result.output == "123456\n"
 
     def test_cli_with_bad_item_name(self):
-        cli = self.build_cli(
-            getpass=lambda prompt: "badger",
-            arguments=("--path", self.keychain_path, "onetos",),
-        )
-
-        self.assert_exit_status(os.EX_DATAERR, cli.run)
-        self.assert_no_output()
-        self.assert_error_output("1pass: Could not find an item named 'onetos'\n")
+        args = ('onetos', '--path', self.keychain_path)
+        with patch('getpass.getpass', lambda prompt: 'badger'):
+            result = self.runner.invoke(cli, args)
+        assert result.exit_code == os.EX_DATAERR
+        assert result.output == "1pass: Could not find an item named 'onetos'\n"
 
     def test_cli_with_fuzzy_matching(self):
-        cli = self.build_cli(
-            getpass=lambda prompt: "badger",
-            arguments=("--fuzzy", "--path", self.keychain_path, "onetos",),
-        )
-        cli.run()
-
-        self.assert_output("123456\n")
-        self.assert_no_error_output()
+        with patch('getpass.getpass', lambda prompt: 'badger'):
+            args = ['--fuzzy', '--path', self.keychain_path, 'onetos']
+            result = self.runner.invoke(cli, args)
+        assert result.exit_code == 0
+        assert result.output == "123456\n"
 
     def test_cli_cancelled_password_prompt(self):
         def keyboard_interrupt(prompt):
             raise KeyboardInterrupt()
-        cli = self.build_cli(
-            getpass=keyboard_interrupt,
-            arguments=("--path", self.keychain_path, "onetosix",),
-        )
 
-        self.assert_exit_status(0, cli.run)
-        self.assert_output("\n")
-        self.assert_no_error_output()
+        args = ('--path', self.keychain_path, 'onetosix')
+        with patch('getpass.getpass', lambda prompt: keyboard_interrupt):
+            result = self.runner.invoke(cli, args)
+        assert result.exit_code == -1
 
     def test_correct_password_from_stdin(self):
-        def flunker(prompt):
-            self.fail("Password prompt was invoked")
-        self.input.write("badger\n")
-        self.input.seek(0)
-        cli = self.build_cli(
-            getpass=flunker,
-            arguments=("--no-prompt", "--path", self.keychain_path, "onetosix",),
-        )
-        cli.run()
-
-        self.assert_output("123456\n")
-        self.assert_no_error_output()
+        args = ('--no-prompt', '--path', self.keychain_path, 'onetosix')
+        with patch('getpass.getpass', lambda prompt: self.password_used):
+            result = self.runner.invoke(cli, args, input='badger\n')
+        assert result.exit_code == 0
+        assert result.output == "123456\n"
 
     def test_incorrect_password_from_stdin(self):
-        def flunker(prompt):
-            self.fail("Password prompt was invoked")
-        self.input.write("wrong-password\n")
-        self.input.seek(0)
-        cli = self.build_cli(
-            getpass=flunker,
-            arguments=("--no-prompt", "--path", self.keychain_path, "onetosix",),
-        )
-
-        self.assert_exit_status(os.EX_DATAERR, cli.run)
-        self.assert_no_output()
-        self.assert_error_output("1pass: Incorrect master password\n")
-
-    def build_cli(self, **kwargs):
-        cli_kwargs = {
-            "stdin": self.input,
-            "stdout": self.output,
-            "stderr": self.error,
-        }
-        cli_kwargs.update(kwargs)
-        return CLI(**cli_kwargs)
-
-    def assert_exit_status(self, expected_status, func):
-        try:
-            func()
-        except SystemExit as exit:
-            self.assertEquals(expected_status, exit.code)
-        else:
-            self.fail("Expected a SystemExit to be raised")
-
-    def assert_output(self, expected_output):
-        self.assertEquals(expected_output, self.output.getvalue())
-
-    def assert_no_output(self):
-        self.assert_output("")
-
-    def assert_error_output(self, expected_output):
-        self.assertEquals(expected_output, self.error.getvalue())
-
-    def assert_no_error_output(self):
-        self.assert_error_output("")
+        args = ('--no-prompt', '--path', self.keychain_path, 'onetosix')
+        with patch('getpass.getpass', lambda prompt: self.password_used):
+            result = self.runner.invoke(cli, args, input='wrong-password\n')
+        assert result.exit_code == os.EX_DATAERR
+        assert result.output == "1pass: Incorrect master password\n"
 
     @property
     def keychain_path(self):
         return os.path.join(os.path.dirname(__file__), "data", "1Password.agilekeychain")
+
+    def password_used(self, prompt):
+        self.fail("Password prompt was invoked")
