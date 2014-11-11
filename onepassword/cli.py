ad1d44a28fa3805b7449237c5dccda7eeeaa419b
George Hickman <george@ghickman.co.uk>
2014-11-11 20:03:40 +0000

    Catch any exceptions raised and raise a ClickException
    ClickException will correctly format any exception message and then exit
    with the correct code: `1`

--- onepassword/cli.py
+++ onepassword/cli.py
@@ -15,22 +15,25 @@ DEFAULT_KEYCHAIN_PATH = "~/Dropbox/1Password.agilekeychain"
 @click.option('--fuzzy', is_flag=True, help="Perform fuzzy matching on the item")
 @click.option('--no-prompt', is_flag=True, help="Don't prompt for a password, read from STDIN instead")
 def cli(item, path, fuzzy, no_prompt):
-    keychain = Keychain(path)
+    try:
+        keychain = Keychain(path)
 
-    if no_prompt:
-        password = sys.stdin.read().strip()
-        keychain.unlock(password)
-        if keychain.locked:
-            click.echo("1pass: Incorrect master password", err=True)
+        if no_prompt:
+            password = sys.stdin.read().strip()
+            keychain.unlock(password)
+            if keychain.locked:
+                click.echo("1pass: Incorrect master password", err=True)
+                sys.exit(os.EX_DATAERR)
+        else:
+            while keychain.locked:
+                keychain.unlock(getpass.getpass("Master password: "))
+
+        found_item = keychain.item(item, 70 if fuzzy else 100)
+
+        if found_item is not None:
+            click.echo(found_item.password)
+        else:
+            click.echo("1pass: Could not find an item named '%s'" % (item), err=True)
             sys.exit(os.EX_DATAERR)
-    else:
-        while keychain.locked:
-            keychain.unlock(getpass.getpass("Master password: "))
-
-    found_item = keychain.item(item, 70 if fuzzy else 100)
-
-    if found_item is not None:
-        click.echo(found_item.password)
-    else:
-        click.echo("1pass: Could not find an item named '%s'" % (item), err=True)
-        sys.exit(os.EX_DATAERR)
+    except Exception as e:
+        raise click.ClickException(e.message)
